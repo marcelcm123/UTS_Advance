85f47725cca20e736792ca28effd5d5d8c564b11
marcelcm123 <liongmarcel@gmail.com>
2021-10-08 08:47:18 +0800

    init

---
+++ app/src/main/java/ac/id/ubaya/uts_food/MainActivity.kt
@@ -0,0 +1,11 @@
+package ac.id.ubaya.uts_food
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
